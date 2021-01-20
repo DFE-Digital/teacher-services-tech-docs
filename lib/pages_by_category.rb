2c694905c504694d2ecc4de36950f5c2fcd08871
Tijmen Brommet <tijmen@gmail.com>
2021-01-20 17:21:20 +0100

    Add page about support to docs

--- lib/pages_by_category.rb
+++ lib/pages_by_category.rb
@@ -7,7 +7,7 @@ class PagesByCategory
 
   def pages
     sitemap.resources
-      .select { |resource| resource.path.end_with?('.html') && resource.data.category }
+      .select { |resource| resource.path.end_with?(".html") && resource.data.category }
       .group_by { |resource| resource.data.category }
   end
 end
