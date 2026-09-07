9b9f7e858a17935bd6adcd29c2b3747a527e192d
David Young <david@dcyoung.dev>
2026-09-07 11:17:22 +0100

    Added tech stack to individual service page

--- config.rb
+++ config.rb
@@ -69,6 +69,9 @@ CS_SERVICE_PROFILES = CS_SERVICE_REPOS.sort_by(&:service_name).map(&:profile)
 RUBY_SERVICE_PROFILES = RUBY_SERVICE_REPOS.sort_by(&:service_name).map(&:profile)
 OTHER_SERVICE_PROFILES = OTHER_SERVICE_REPOS.sort_by(&:service_name).map(&:profile)
 RUBY_SERVICE_PROFILES_BY_NAME = RUBY_SERVICE_PROFILES.to_h { |profile| [profile.service_name, profile] }.freeze
+CS_SERVICE_PROFILES_BY_NAME = CS_SERVICE_PROFILES.to_h { |profile| [profile.service_name, profile] }.freeze
+OTHER_SERVICE_PROFILES_BY_NAME = OTHER_SERVICE_PROFILES.to_h { |profile| [profile.service_name, profile] }.freeze
+SERVICE_LANGUAGES_BY_NAME = service_list.to_h { |service| [service["name"], service["language"]] }.freeze
 SERVICE_DOCS = service_docs.freeze
 SERVICE_PAGE_PATHS = service_list.to_h { |service| [service["name"], "/service/#{service_page_slug.call(service["name"])}.html"] }.freeze
 
@@ -129,7 +132,10 @@ service_list.each do |service|
     service_name: service["name"],
     repo_name: service["repo_name"],
     pages: SERVICE_DOCS.fetch(service["name"], []),
+    service_language: SERVICE_LANGUAGES_BY_NAME.fetch(service["name"]),
     ruby_service_profile: RUBY_SERVICE_PROFILES_BY_NAME[service["name"]],
+    cs_service_profile: CS_SERVICE_PROFILES_BY_NAME[service["name"]],
+    other_service_profile: OTHER_SERVICE_PROFILES_BY_NAME[service["name"]],
   }, data: {
     title: service["name"],
   }
