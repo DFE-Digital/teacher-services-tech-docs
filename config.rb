d4f846db9f368ce4b771a3c603f5ce5df55b3bd6
David Young <david@dcyoung.dev>
2026-09-07 11:02:26 +0100

    Added tech stack to individual service page

--- config.rb
+++ config.rb
@@ -68,6 +68,7 @@ end
 CS_SERVICE_PROFILES = CS_SERVICE_REPOS.sort_by(&:service_name).map(&:profile)
 RUBY_SERVICE_PROFILES = RUBY_SERVICE_REPOS.sort_by(&:service_name).map(&:profile)
 OTHER_SERVICE_PROFILES = OTHER_SERVICE_REPOS.sort_by(&:service_name).map(&:profile)
+RUBY_SERVICE_PROFILES_BY_NAME = RUBY_SERVICE_PROFILES.to_h { |profile| [profile.service_name, profile] }.freeze
 SERVICE_DOCS = service_docs.freeze
 SERVICE_PAGE_PATHS = service_list.to_h { |service| [service["name"], "/service/#{service_page_slug.call(service["name"])}.html"] }.freeze
 
@@ -128,6 +129,7 @@ service_list.each do |service|
     service_name: service["name"],
     repo_name: service["repo_name"],
     pages: SERVICE_DOCS.fetch(service["name"], []),
+    ruby_service_profile: RUBY_SERVICE_PROFILES_BY_NAME[service["name"]],
   }, data: {
     title: service["name"],
   }
