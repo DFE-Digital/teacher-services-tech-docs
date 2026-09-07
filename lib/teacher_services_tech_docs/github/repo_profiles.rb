d8e27c06ca5e531773a98773d8416b62ba0f9069
David Young <david@dcyoung.dev>
2026-09-07 13:35:20 +0100

    Rearranged modules and classes

--- lib/teacher_services_tech_docs/github/repo_profiles.rb
+++ lib/teacher_services_tech_docs/github/repo_profiles.rb
@@ -31,59 +31,6 @@ module SchoolsDigitalTechDocs
         end
       end
 
-      class BasicRepoProfile < Data.define(:service_name, :repo_name, :repo, :language)
-        include BaseProfile
-
-        def tech_stack
-          {
-            "language" => language,
-          }
-        end
-      end
-
-      class RubyRepoProfile < Data.define(:service_name, :repo_name, :repo, :dependencies)
-        include BaseProfile
-        extend Forwardable
-
-        def_delegators :dependencies, :rails_version, :ruby_version, :dfe_analytics_version, :dfe_reference_data_version, :dfe_autocomplete_version
-        def_delegator :dependencies, :has_tool_versions?, :asdf?
-
-        def language
-          "ruby"
-        end
-
-        def tech_stack
-          {
-            "rails" => rails_version,
-            "ruby" => ruby_version,
-            "dfe-analytics" => dfe_analytics_version,
-            "dfe-reference-data" => dfe_reference_data_version,
-            "dfe-autocomplete" => dfe_autocomplete_version,
-          }
-        end
-      end
-
-      class CsRepoProfile < Data.define(:service_name, :repo_name, :repo, :dependencies, :asdf)
-        include BaseProfile
-        extend Forwardable
-
-        def_delegators :dependencies, :target_framework, :dfe_analytics_version
-
-        def language
-          "cs"
-        end
-
-        def asdf?
-          asdf
-        end
-
-        def tech_stack
-          {
-            "framework" => target_framework,
-            "dfe-analytics-net" => dfe_analytics_version,
-          }
-        end
-      end
     end
   end
 end
---
+++ lib/teacher_services_tech_docs/github/ruby/profile.rb
@@ -0,0 +1,27 @@
+module SchoolsDigitalTechDocs
+  module GitHub
+    module RepoProfiles
+      class RubyRepoProfile < Data.define(:service_name, :repo_name, :repo, :dependencies)
+        include BaseProfile
+        extend Forwardable
+
+        def_delegators :dependencies, :rails_version, :ruby_version, :dfe_analytics_version, :dfe_reference_data_version, :dfe_autocomplete_version
+        def_delegator :dependencies, :has_tool_versions?, :asdf?
+
+        def language
+          "ruby"
+        end
+
+        def tech_stack
+          {
+            "rails" => rails_version,
+            "ruby" => ruby_version,
+            "dfe-analytics" => dfe_analytics_version,
+            "dfe-reference-data" => dfe_reference_data_version,
+            "dfe-autocomplete" => dfe_autocomplete_version,
+          }
+        end
+      end
+    end
+  end
+end
